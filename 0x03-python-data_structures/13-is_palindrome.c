2f7b1533fcfdbb7fb5e959c28bc9dddff29f0f83
Daerego Braide <phronesis4xt@gmail.com>
2022-09-24 17:05:46 -0700

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,13 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 /**
- *  * main - check the code for Holberton School students.
- *   *
- *    * Return: Always 0.
+ *  * is_palindrome - prints all elements of a listint_t list
+ *   * @head: pointer to head of list
+ *    * Return: number of nodes
 */
-int main(void)
+int is_palindrome(listint_t **head)
 {
-listint_t *head;
+listint_t *temp = *head, *temp2 = *head;
+int array[5000];
+int len, j, i;
+if (!head)
 return (0);
+if (!*head || ((*head)->next == NULL))
+return (1);
+for (len = 0; temp2->next != NULL; len++)
+{
+temp2 = temp2->next;
+}
+for (i = 0; i <= len; i++)
+{
+array[i] = temp->n;
+temp = temp->next;
+}
+for (j = 0, len; j < len; j++, len--)
+{
+if (array[j] != array[len])
+{
+return (0);
+}
+}
+return (1);
 }
