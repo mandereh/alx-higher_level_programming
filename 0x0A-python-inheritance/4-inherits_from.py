93b68cb795fc4b419804f7803e0f76bc24fe68f3
Daerego Braide <phronesis4xt@gmail.com>
2022-10-24 16:27:36 -0700

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-''' module: 4-inherits_from
-'''
+"""Function that inherits"""
 
 
 def inherits_from(obj, a_class):
-    '''the object is an instance of a class that inherited (directly or indirectly)
-    obj: an object
-    a_class: a class
-    returns None
-    '''
-    return type(obj) != a_class and isinstance(obj, a_class)
+    """Function that inherits"""
+
+    if type(obj) is a_class or not isinstance(obj, a_class):
+        return False
+    else:
+        return isinstance(obj, a_class)
